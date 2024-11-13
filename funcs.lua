277a197429de30d3b68a4104385747459ae909b0
TheXRify <penguinking684@gmail.com>
2024-11-13 11:57:14 -0600

    resource creation

--- funcs.lua
+++ funcs.lua
@@ -3,5 +3,6 @@ local module = {}
 module.remove_techs = require "lib.funcs.remove_techs"
 module.remove_recipes = require "lib.funcs.remove_recipes"
 module.remove_ores = require "lib.funcs.remove_ores"
+module.create_resource = require "lib.funcs.create_resource"
 
 return module
