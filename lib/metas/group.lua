39f95f510e90164d7c8cb658dfadcb3e4600444a
TheXRify <penguinking684@gmail.com>
2024-11-17 21:00:57 -0600

    easier group creation

---
+++ lib/metas/group.lua
@@ -0,0 +1,27 @@
+GROUP = setmetatable({}, {
+    __call = function(self, group)
+        if not group.type then
+            error("Tried to extend a group " .. group.name .. " without providing a type ")
+        end
+        
+        ext_group = {
+            type = group.type,
+            name = group.name,
+            icon = group.icon,
+            order = group.order or "a"
+        }
+
+        data:extend{ext_group}
+
+        for _,sg in pairs(group.subs) do
+            subgroup = {
+                type = "item-subgroup",
+                name = sg.name,
+                group = group.name
+            }
+            data:extend{subgroup}
+        end
+
+        return group
+    end
+})
